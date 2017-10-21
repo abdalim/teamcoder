5d2c3be780a42dc17c98afae6fc274f1e4f134f0
Alim <blindspin@gmail.com>
2017-10-21 22:38:55 +0800

    Add specs

---
+++ test/test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+describe('teamcoder', function () {
+  context.skip('given a single team name', function () {
+    it('should return first 3 chars')
+    it('should return human readable team code')
+  })
+
+  context.skip('given a list of team names', function () {
+    it('should return list of team codes without duplicates')
+  })
+})
