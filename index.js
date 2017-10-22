315c9e2a164052bd77b6a794f201b71c0a6c026a
Alim <blindspin@gmail.com>
2017-10-22 11:02:51 +0800

    Add unsupported input handling

--- index.js
+++ index.js
@@ -3,11 +3,16 @@
 const START_INDEX = 0
 const NUM_OF_CHARS = 3
 
+function isBadTeamName (teamName) {
+  return !teamName || typeof teamName === 'object'
+}
+
 /**
  * Generates a team code out of a team name
  * @param {string} teamName : a team's name
  * @return {string} generated team code
  */
 module.exports = function (teamName) {
+  if (isBadTeamName(teamName)) return ''
   return teamName.toString().substring(START_INDEX, NUM_OF_CHARS).toUpperCase()
 }
